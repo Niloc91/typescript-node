9520b2db672695d7ab81a66dbb69623e87335294
colin <colinsiew91@gmail.com>
2019-02-21 14:26:45 +0100

    added express-prom-bundle

--- src/index.ts
+++ src/index.ts
@@ -9,21 +9,29 @@ const port = 3001;
 const prom = require('prom-client');
 const prom_gc = require('prometheus-gc-stats');
 prom_gc();
+const promBundle = require("express-prom-bundle");
+const metricsMiddleware = promBundle({includeMethod: true,includePath:true});
 
 const Monitor = require('./Monitor');
 
+app.get('/metrics', function(req, res) {
+    res.end(prom.register.metrics());
+});
+
+
+
+
+app.get('/ignored', (req, res) => res.send('This path is ignored in prometheus'));
+app.get('/test', (req, res) => res.send('This path is also ignored'));
+//Paths above are ignored by the counter
+app.use(metricsMiddleware);
 app.get('/', (req, res) => res.send('Hello World!'));
-app.get('/greeter', (req, res) => res.send(Greeter('Colin2')));
+app.get('/greeter', (req, res) => res.send(Greeter('Colin')));
 app.get('/adder', (req, res) => res.send(Examplefunctions.adder(1,5).toString()));
 app.get('/count', (req, res) => res.send(Examplefunctions.countevens(12).toString()));
 app.get('/cast', (req, res) => res.send(Examplefunctions.typeAssertion("1","3").toString()));
 app.get('/wait', (req, res) => res.send(Promise.resolve(Asyncexamples.addasync()).toString()));
 
-
-
-app.get('/metrics', function(req, res) {
-    res.end(prom.register.metrics());
-});
 
 app.use(Monitor.requestCounters);
 app.use(Monitor.responseCounters);
